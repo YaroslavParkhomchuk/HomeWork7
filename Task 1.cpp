53063f3b531cc29168d4f6357d1efd4320ffa7d2
YaroslavParkhomchuk <92468401+YaroslavParkhomchuk@users.noreply.github.com>
2021-11-16 03:28:04 +0200

    Update Task 1.cpp

--- Task 1.cpp
+++ Task 1.cpp
@@ -5,51 +5,48 @@ using namespace std;
 class Human
 {
 public:
-	Human() {};
 	virtual void print_info() const = 0;
-	virtual void print_name() const = 0;
+	void print_name()
+	{
+		cout << "Name: " << name << "\n";
+	}
 	virtual ~Human() {};
+
+protected:
+	string name;
+	string subject;
+	Human(string name1, string subject1) : name(name1), subject(subject1) {};
 };
 
 class Teacher : public Human
 {
 public:
-	Teacher(string name1, string profession1, string subject1) : 
-		name(name1), profession(profession1), subject(subject1) {}
-	void print_name() const
-	{
-		cout << "Name of teacher: " << name << endl;
-	}
+	Teacher(string name1, string profession1, string subject1) 
+		: Human(name1, subject1), profession(profession1) {}
 	void print_info() const 
 	{
-		cout << "Profession: " << profession << "\nTeaches: " << subject << endl;
+		cout << "Profession: " << profession << "\nTeaches: " << subject << "\n\n";
 	}
+	
 private:
-	string name;
 	string profession;
-	string subject;
 };
+
 class Student : public Human
 {
 public:
-	Student(string name2, string subject2): name(name2), subject(subject2) {}
-	void print_name() const
-	{
-		cout << "\nName of student: " << name << endl;
-	}
+	Student(string name2, string subject2) : Human(name2, subject2) {}
 	void print_info() const
 	{
-		cout << "Learns: " << subject << endl;
+		cout << "Learns: " << subject << "\n";
 	}
-private:
-	string name;
-	string subject;
 }; 
 
 int main()
 {
-	Teacher teacher("John", "Teacher", "Physic");
-	Student student("Max", "Mathematic");
+	Teacher teacher("John", "Teacher", "Physics");
+	Student student("Max", "Mathematics");
 	teacher.print_name(); teacher.print_info();
 	student.print_name(); student.print_info();
+	return 0;
 }
